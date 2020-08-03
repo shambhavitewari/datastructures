f3c9c5fea460304aa16b0371dc1bcfb22fdc2134
Shambhavi Tewari <33371065+shambhavitewari@users.noreply.github.com>
2020-08-03 13:47:49 +0530

    Add files via upload

---
+++ remove-dupicates-from-sorted-list.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        
+        ListNode trav = head;
+        
+        while(trav!=null && trav.next!=null)
+        {
+            while(trav!=null && trav.next!=null && trav.val==trav.next.val)
+            {
+                trav.next=trav.next.next;
+            }
+            
+            trav=trav.next;
+        }
+        
+        return head;
+        
+    }
+}
